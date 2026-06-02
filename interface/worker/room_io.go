e4918466a3a37a667b21a9937874ed6cbe4d8e90
Ramdhan Hidayat <ramdhan@cavos.io>
2026-06-02 21:37:30 +0700

    fix(agent): wire audio frame helpers

--- interface/worker/room_io.go
+++ interface/worker/room_io.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/cavos-io/rtp-agent/core/agent"
+	"github.com/cavos-io/rtp-agent/core/audio"
 	"github.com/cavos-io/rtp-agent/core/audio/model"
 	"github.com/cavos-io/rtp-agent/library/logger"
 	"github.com/hraban/opus"
@@ -805,8 +806,7 @@ func (rio *RoomIO) PublishAudio(frame *model.AudioFrame) error {
 		}
 	}
 
-	// Calculate duration based on sample rate and samples
-	duration := time.Duration(frame.SamplesPerChannel) * time.Second / time.Duration(frame.SampleRate)
+	duration := time.Duration(audio.CalculateFrameDuration(frame) * float64(time.Second))
 
 	return track.WriteSample(media.Sample{
 		Data:     data,
