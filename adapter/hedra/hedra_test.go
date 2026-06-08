2016f370538da0d54200d854703ed4878cd9347f
Ramdhan Hidayat <ramdhan@cavos.io>
2026-06-08 20:51:17 +0700

    fix(hedra): report disabled avatar service

--- adapter/hedra/hedra_test.go
+++ adapter/hedra/hedra_test.go
@@ -1,6 +1,10 @@
 package hedra
 
-import "testing"
+import (
+	"context"
+	"strings"
+	"testing"
+)
 
 func TestHedraPluginMetadataUsesRTPAgentNamespace(t *testing.T) {
 	if PluginTitle != "rtp-agent.plugins.hedra" {
@@ -11,5 +15,18 @@ func TestHedraPluginMetadataUsesRTPAgentNamespace(t *testing.T) {
 	}
 	if PluginPackage != "rtp-agent.plugins.hedra" {
 		t.Fatalf("PluginPackage = %q, want rtp-agent.plugins.hedra", PluginPackage)
+	}
+}
+
+func TestHedraAvatarStartReportsDisabledReferenceService(t *testing.T) {
+	avatar := NewHedraAvatar("test-key")
+
+	err := avatar.Start(context.Background())
+
+	if err == nil {
+		t.Fatal("Start() error = nil, want disabled service error")
+	}
+	if !strings.Contains(err.Error(), "hedra realtime avatar service has been disabled") {
+		t.Fatalf("Start() error = %q, want disabled reference service message", err)
 	}
 }
