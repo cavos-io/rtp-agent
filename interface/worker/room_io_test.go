73032db634d8453f3262c47ec5cca3cec87fa207
Ramdhan Hidayat <ramdhan@cavos.io>
2026-06-17 01:08:30 +0700

    feat(worker): pause room audio output

--- interface/worker/room_io_test.go
+++ interface/worker/room_io_test.go
@@ -464,6 +464,49 @@ func TestRoomIOPublishAudioHonorsCanceledContextBeforeEncoding(t *testing.T) {
 	}
 }
 
+func TestRoomIOPauseAudioOutputDefersPublishUntilResume(t *testing.T) {
+	encoder := &recordingRoomIOEncoder{encoded: []byte{0x01, 0x02}}
+	rio := &RoomIO{
+		audioTrack: newRoomIOTestAudioTrack(t),
+		encoder:    encoder,
+	}
+	frame := &model.AudioFrame{
+		Data:              make([]byte, 960*2),
+		SampleRate:        48000,
+		NumChannels:       1,
+		SamplesPerChannel: 960,
+	}
+
+	rio.PauseAudioOutput()
+	published := make(chan error, 1)
+	go func() {
+		published <- rio.PublishAudio(context.Background(), frame)
+	}()
+
+	select {
+	case err := <-published:
+		t.Fatalf("PublishAudio returned while paused with error %v", err)
+	case <-time.After(20 * time.Millisecond):
+	}
+	if len(encoder.calls) != 0 {
+		t.Fatalf("encoder calls = %d, want 0 while audio output is paused", len(encoder.calls))
+	}
+
+	rio.ResumeAudioOutput()
+
+	select {
+	case err := <-published:
+		if err != nil {
+			t.Fatalf("PublishAudio after resume error = %v", err)
+		}
+	case <-time.After(time.Second):
+		t.Fatal("PublishAudio did not resume")
+	}
+	if len(encoder.calls) != 1 {
+		t.Fatalf("encoder calls = %d, want 1 after resume", len(encoder.calls))
+	}
+}
+
 func TestRoomIOClearBufferFinishesPlaybackAsInterrupted(t *testing.T) {
 	rio := &RoomIO{audioTrack: newRoomIOTestAudioTrack(t)}
 	frame := &model.AudioFrame{
