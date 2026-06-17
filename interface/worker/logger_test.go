e8a7c240618ba37089af63d8f703b2a9e60ba6fa
Ramdhan Hidayat <ramdhan@cavos.io>
2026-06-17 19:58:44 +0700

    refactor(worker): split livekit room io package

---
+++ interface/worker/logger_test.go
@@ -0,0 +1,38 @@
+package worker
+
+import livekitlogger "github.com/livekit/protocol/logger"
+
+type roomIORecordingLogger struct {
+	warnMessages []string
+}
+
+func (l *roomIORecordingLogger) Debugw(string, ...any) {}
+func (l *roomIORecordingLogger) Infow(string, ...any)  {}
+func (l *roomIORecordingLogger) Warnw(msg string, err error, keysAndValues ...any) {
+	l.warnMessages = append(l.warnMessages, msg)
+}
+func (l *roomIORecordingLogger) Errorw(string, error, ...any) {}
+func (l *roomIORecordingLogger) WithValues(keysAndValues ...any) livekitlogger.Logger {
+	return l
+}
+func (l *roomIORecordingLogger) WithUnlikelyValues(keysAndValues ...any) livekitlogger.UnlikelyLogger {
+	return livekitlogger.GetDiscardLogger().WithUnlikelyValues(keysAndValues...)
+}
+func (l *roomIORecordingLogger) WithName(name string) livekitlogger.Logger {
+	return l
+}
+func (l *roomIORecordingLogger) WithComponent(component string) livekitlogger.Logger {
+	return l
+}
+func (l *roomIORecordingLogger) WithCallDepth(depth int) livekitlogger.Logger {
+	return l
+}
+func (l *roomIORecordingLogger) WithItemSampler() livekitlogger.Logger {
+	return l
+}
+func (l *roomIORecordingLogger) WithoutSampler() livekitlogger.Logger {
+	return l
+}
+func (l *roomIORecordingLogger) WithDeferredValues() (livekitlogger.Logger, livekitlogger.DeferredFieldResolver) {
+	return livekitlogger.GetDiscardLogger().WithDeferredValues()
+}
