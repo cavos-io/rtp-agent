1be62513f2a87dada509ca301c4eebddf01db362
Ramdhan Hidayat <ramdhan@cavos.io>
2026-06-25 00:00:19 +0700

    fix(fishaudio): preserve queued tts audio

--- adapter/fishaudio/tts.go
+++ adapter/fishaudio/tts.go
@@ -608,6 +608,19 @@ func (s *fishAudioTTSSynthesizeStream) Next() (*tts.SynthesizedAudio, error) {
 	}
 	select {
 	case audio, ok := <-s.events:
+		if ok {
+			return audio, nil
+		}
+		select {
+		case err := <-s.errCh:
+			return nil, err
+		default:
+			return nil, io.EOF
+		}
+	default:
+	}
+	select {
+	case audio, ok := <-s.events:
 		if !ok {
 			select {
 			case err := <-s.errCh:
