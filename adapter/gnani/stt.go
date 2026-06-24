f707465d7c4ae17660f6cb1811f11793030bb9e3
Ramdhan Hidayat <ramdhan@cavos.io>
2026-06-24 22:05:17 +0700

    fix(gnani): order stt transcript before error

--- adapter/gnani/stt.go
+++ adapter/gnani/stt.go
@@ -346,6 +346,13 @@ func (s *gnaniSTTStream) Next() (*stt.SpeechEvent, error) {
 		}
 		return event, nil
 	case err := <-s.errCh:
+		select {
+		case event, ok := <-s.events:
+			if ok {
+				return event, nil
+			}
+		default:
+		}
 		return nil, err
 	case <-s.ctx.Done():
 		if s.isClosed() {
