60f34fa3d635b41b923688830352f57f395f416c
Ramdhan Hidayat <ramdhan@cavos.io>
2026-06-24 21:57:59 +0700

    fix(smallestai): order stt transcript before error

--- adapter/smallestai/stt.go
+++ adapter/smallestai/stt.go
@@ -810,6 +810,13 @@ func (s *smallestAISTTStream) Next() (*stt.SpeechEvent, error) {
 		}
 		return event, nil
 	case err := <-s.errCh:
+		select {
+		case event, ok := <-s.events:
+			if ok {
+				return event, nil
+			}
+		default:
+		}
 		return nil, err
 	case <-s.ctx.Done():
 		if s.isClosed() {
