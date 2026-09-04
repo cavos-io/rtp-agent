c02bf7ee792c77ed489cf7c5c9210899aab36c12
Vinsensius Angelo <vafrcor2007@yahoo.co.id>
2026-09-04 15:20:09 +0700

    fix(test): fix lefthook ci

--- adapter/google/stt.go
+++ adapter/google/stt.go
@@ -2426,6 +2426,7 @@ func (s *googleSTTStream) PushFrame(frame *model.AudioFrame) error {
 	}
 	if frame != nil && frame.SampleRate != 0 {
 		if s.pushedSampleRate != 0 && s.pushedSampleRate != frame.SampleRate {
+			s.mu.Unlock()
 			return errors.New("the sample rate of the input frames must be consistent")
 		}
 		s.pushedSampleRate = frame.SampleRate
